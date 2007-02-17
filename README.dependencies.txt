a3ba4d68e6c8df78edfa704764d124da1abc7b17
ceharris414 <ceharris414@56abbbcf-a929-0410-a3b1-a59216e82f73>
2007-02-17 14:12:17 +0000

    Documented build-time and run-time dependencies

---
+++ README.dependencies.txt
@@ -0,0 +1,34 @@
+
+Library Component Dependencies for the Soulwing CAS Client
+----------------------------------------------------------
+
+JAR files for all of the dependent libraries are in the "vendor" folder.
+
+commons-logging.jar (run-time, required)
+    Apache Commons logging.
+ 
+confluence-2.3.1.jar (build-time)
+		Supporting classes needed to compile the application adapter for 
+		Atlassian Confluence.
+
+jakarta-oro-2.0.8.jar (run-time, optional)
+		Provides glob pattern matching support for the CAS bypass filter.
+		If you're not using CAS bypass, you can omit this library.
+		
+jdom.jar (run-time, required)
+		Used by the CAS client classes to interpret CAS responses.
+
+junit-4.1.jar (build-time)
+		Provide support for unit tests in "test" folder.
+				
+seraph-0.7.23.jar (build-time)
+		Supporting classes needed to compile the application adapters for 
+		Atlassian JIRA and Confluence
+
+servlet-api.jar (build-time)
+		Supporting classes for Servlet API version 2.4
+
+xercesImpl.jar (run-time, option)
+		Xerces JAXP parser implementation.  You can use a different JAXP
+		parser, if you prefer -- JDOM uses the standard JAXP API to obtain
+		an XML parser.
---
+++ src/org/soulwing/cas/apps/atlassian/ConfluenceCasAuthenticator.java
@@ -0,0 +1,104 @@
+/*
+ * ConfluenceCasAuthenticator.java
+ *
+ * Created on Feb 12, 2007
+ *
+ * Copyright (C) 2006, 2007 Carl E Harris, Jr.
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * This software was derived substantially from the CasSeraphAuthenticator
+ * class developed by Ingomar Otter (ingomar.otter@valtech.de) as modified by
+ * Jason Shao (jayshao.rutgers.edu).  Thanks to the both of them for making
+ * their work freely available to public.
+ */
+
+package org.soulwing.cas.apps.atlassian;
+
+
+import java.security.Principal;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.soulwing.cas.client.ServiceValidationResponse;
+import org.soulwing.cas.support.ValidationUtils;
+
+import com.atlassian.confluence.user.ConfluenceAuthenticator;
+import com.atlassian.seraph.auth.AuthenticatorException;
+import com.atlassian.seraph.config.SecurityConfig;
+
+
+/**
+ * Subclass of ConfluenceAuthenticator that provides CAS authentication for
+ * Confluence.
+ *
+ * @author ingomar.otter
+ * @author jayshao
+ * @author Carl Harris
+ * 
+ */
+public class ConfluenceCasAuthenticator extends ConfluenceAuthenticator {
+
+  private static final long serialVersionUID = 1L;
+  private static final Log log = 
+      LogFactory.getLog(ConfluenceCasAuthenticator.class);
+
+  public void init(Map params, SecurityConfig config) {
+    super.init(params, config);
+    
+    log.debug("initialized with loginURL=" + config.getLoginURL()
+        + " and logoutURL=" + config.getLogoutURL());
+    log.debug("param keys=" + params.keySet());
+  }
+
+  public Principal getUser(HttpServletRequest request, 
+      HttpServletResponse response) {
+
+    ServiceValidationResponse validation =
+        ValidationUtils.getServiceValidationResponse(request);
+    if (validation == null) {
+      log.debug("no CAS validation");
+      return super.getUser(request, response);
+    }
+    log.debug("CAS validation for user " + validation.getUserName());
+    Principal user = super.getUser(validation.getUserName());
+    request.getSession().setAttribute(LOGGED_IN_KEY, user);
+    request.getSession().setAttribute(LOGGED_OUT_KEY, null);
+    return user;
+  }
+
+  public boolean login(HttpServletRequest request,
+      HttpServletResponse response, String username, String password) 
+      throws AuthenticatorException {
+    return login(request, response, username, password, false);
+  }
+
+  public boolean login(HttpServletRequest request,
+      HttpServletResponse response, String username, 
+      String password, boolean cookie) 
+      throws AuthenticatorException {
+    log.debug("login invoked");
+    return super.login(request, response, username, password, cookie);
+  }
+
+  public boolean logout(HttpServletRequest request,
+      HttpServletResponse response) throws AuthenticatorException {
+    log.debug("logout invoked");
+    request.getSession().setAttribute(LOGGED_IN_KEY, null);
+    request.getSession().setAttribute(LOGGED_OUT_KEY, new Boolean(true));
+    return true;
+  }
+ 
+}
