8a45338d2315abd9aa8ca28402a08d58e244f446
ceharris414 <ceharris414@56abbbcf-a929-0410-a3b1-a59216e82f73>
2011-02-25 12:11:39 +0000

    added CasContext class

---
+++ src/org/soulwing/cas/apps/tomcat/CasContext.java
@@ -0,0 +1,41 @@
+/*
+ * CasContext.java
+ *
+ * Created on Feb 25, 2011
+ *
+ * Copyright (C) 2011 Carl E Harris, Jr.
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ */
+package org.soulwing.cas.apps.tomcat;
+
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.deploy.LoginConfig;
+
+/**
+ * A {@link StandardContext} that uses overrides the {@code <login-config>} 
+ * specified in {@code web.xml}, using CAS instead.
+ *
+ * @author Carl Harris
+ */
+public class CasContext extends StandardContext {
+
+  private static final long serialVersionUID = 2295536636488213569L;
+
+  private static final String CAS_AUTH_METHOD = "CAS";
+
+  public LoginConfig getLoginConfig() {
+    LoginConfig config = super.getLoginConfig();
+    config.setAuthMethod(CAS_AUTH_METHOD);
+    return config;
+  }
+  
+}
